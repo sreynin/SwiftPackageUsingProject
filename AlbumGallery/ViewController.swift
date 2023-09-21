121749a5727b36a15a3573aceae496885b94ee69
sreinin <bosreynin7@gmail.com>
2023-09-21 09:02:56 +0700

    add data to profileVC by use profileModel from package

--- AlbumGallery/ViewController.swift
+++ AlbumGallery/ViewController.swift
@@ -29,7 +29,8 @@ class ViewController: UIViewController {
     @IBAction func clickMeAction(_ sender: UIButton) {
         sender.defaultNativeFeelingAnimation(scaleMeasurement: 0.8){_ in
             self.customview_library.showAlert(title: "Hello", message: "Do you want to learn Swift package manager?", completion: {
-              let profileVC = CustomView_Library.createProfileViewController()
+                let profileData = ProfileModel(imgUrl: "https://play-lh.googleusercontent.com/0SAFn-mRhhDjQNYU46ZwA7tz0xmRiQG4ZuZmuwU8lYmqj6zEpnqsee_6QDuhQ4ZofwXj=w240-h480-rw", name: "Sreinin", dateBirth: "1995.05.15", phoneNum:"01067049666" , addr: "부산광역시 동구 초량동 1226 범양레우스 센트럴베이, 센트럴베이 103동 3203호")
+                let profileVC = CustomView_Library.createProfileViewController(profileData: profileData)
               self.navigationController?.pushViewController(profileVC, animated: true)
           })
         }
